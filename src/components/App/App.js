e7cf43de8722952f7a85f93e838d75c051ac6afd
Maks <kiva_hill@mail.ru>
2023-03-29 13:46:49 +0300

    feat(src): Finish project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,20 @@
 import CreateTask from "../CreateTask/CreateTask";
 import Task from "../Task/Task";
 import Footer from "../Footer/Footer";
-import {useState} from "react";
+import {useCallback, useState} from "react";
 
 function App() {
   const[task, setTask] = useState([])
 
   const getNewTask = (text) => setTask([...task, text])
   const allClear = () => setTask([])
+  const deletTask = (i) => setTask(task.filter((el, index) =>  index !== i))
 
   return (
     <>
       <main>
         <CreateTask getNewTask={getNewTask} />
-        <Task allTasks={task}/>
+        <Task allTasks={task} deletTask={deletTask}/>
       </main>
       <Footer
         countTasks={task.length}
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,17 +1,18 @@
 import './Task.css'
-import arrow from '../../static/icons/close.svg'
-import {useEffect, useState} from "react";
-import NoTask from "../NoTask/NoTask";
+import arrowSvg from '../../static/icons/close.svg'
+import doneSvg from '../../static/icons/done.svg'
+import {useEffect, useState} from 'react';
+import NoTask from '../NoTask/NoTask';
 
 function Task(props) {
-  const{allTasks} = props
+  const{allTasks, deletTask} = props
   const[tasks, setTasks] = useState([])
 
   useEffect(() => {
     setTasks(allTasks)
   }, [allTasks])
 
-  const todo = tasks.length === 0 ? <NoTask/> : <Todo tasks={tasks}/>
+  const todo = tasks.length === 0 ? <NoTask/> : <Todo tasks={tasks} deletTask={deletTask}/>
 
   return (
     <section className='task container'>
@@ -23,7 +24,12 @@ function Task(props) {
 }
 
 function Todo(props) {
-  const{tasks} = props
+  const{tasks, deletTask} = props
+  const doneTask = (event) => {
+    if (event.target.classList.contains('task__close')) return
+    const item = event.target.closest('.task__item')
+    item.classList.toggle('task__item_done')
+  }
 
   return (
     (
@@ -31,13 +37,21 @@ function Todo(props) {
         <li
           key={index}
           className='task__item grid-row justify-content-start'
+          onClick={doneTask}
         >
-          <div className="task__circle"></div>
+          <div className='task__circle'>
+            <img
+              className='task__done icon-16'
+              src={doneSvg}
+              alt=''
+            />
+          </div>
           <p className='task__text'>{task}</p>
           <img
             className='task__close icon-24'
-            src={arrow}
+            src={arrowSvg}
             alt=''
+            onClick={() => deletTask(index)}
           />
         </li>
       )
